1f6dfe907f86e61549d753948e69da3a4217c4bb
SUBAGOVINDHAN <163967881+SUBAGOVINDHAN@users.noreply.github.com>
2026-07-11 11:11:37 +0530

    fix: admin image URL for production

--- Zen/petstore/backend/config/db.js
+++ Zen/petstore/backend/config/db.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'pet',
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 2,
   queueLimit: 0,
   family: 4 
 });
@@ -21,7 +21,7 @@ export const testConnection = async () => {
     conn.release();
   } catch (err) {
     console.error('❌ MySQL Connection failed:', err.message);
-    process.exit(1);
+    
   }
 };
 
